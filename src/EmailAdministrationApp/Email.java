d9307edd8420e0b28d6ee2d014420db7e0a55359
BarenneM <barenne.marie@outlook.fr>
2023-05-02 19:18:28 +0200

    initialization of main methode and object

---
+++ src/EmailAdministrationApp/Email.java
@@ -0,0 +1,23 @@
+package EmailAdministrationApp;
+
+public class Email {
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+
+    //Constructor
+
+    //Ask for the department
+
+    //Generate randome password
+
+    //Set the mailBoxCapacity
+
+    //Set the alternateEmail
+
+    //Change password
+
+}
